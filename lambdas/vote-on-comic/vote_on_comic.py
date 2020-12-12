8f1e8fb742849040ea99c9fca7a62596f78235a5
Landon Shumway <landon.shumway@familysearch.org>
2020-12-12 07:07:09 -0600

    More flesh added to vote endpoint
    still needs to actually update the vote, but it's closer :)

--- lambdas/vote-on-comic/vote_on_comic.py
+++ lambdas/vote-on-comic/vote_on_comic.py
@@ -1,4 +1,6 @@
 import json
+from common.comic_table import ComicTable
+import common.okta_helper as okta_helper
 
 
 def vote_on_comic_panel_handler(event, context):
@@ -10,7 +12,38 @@ def vote_on_comic_panel_handler(event, context):
 
     print(event["pathParameters"])
 
+    comic_id = event["pathParameters"].get("comicId")
+    panel_id = event["body"].get("panelId")
+    if not comic_id or not panel_id:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "missing required fields"}),
+        }
+
+    auth_header = event["headers"].get("Authorization", "")
+    user_profile = okta_helper.get_user_profile(auth_header)
+    if not user_profile:
+        return {
+            "statusCode": 403,
+            "body": json.dumps({"message": "not authorized"}),
+        }
+
+    user_id = user_profile["sub"]
+
+    comic_table = ComicTable()
+
+    comic_data = comic_table.get_comic(comic_id)
+
+    updated_comic = _add_user_to_panel_votes(comic_data, user_id, panel_id)
+
+    comic_table.put_comic(updated_comic)
+
     return {
         "statusCode": 200,
-        "body": json.dumps({"comicId": "12345"}),
+        "body": json.dumps({"comicId": comic_id}),
     }
+
+
+def _add_user_to_panel_votes(comic_data, user_id, panel_id):
+    # TODO find panel and add user id to it
+    return comic_data
