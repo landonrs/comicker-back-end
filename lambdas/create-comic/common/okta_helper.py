3ad172e0857c831ef6c5b9723eed52ce12c85bd7
Landon Shumway <landon.shumway@familysearch.org>
2020-12-17 08:41:15 -0600

    Fix user info endpoint
    apparently it uses a different endpoint when you authenticate
    using the default oauth path...
    
    startup script now always builds before starting

--- lambdas/create-comic/common/okta_helper.py
+++ lambdas/create-comic/common/okta_helper.py
@@ -1,7 +1,7 @@
 import requests
 
 OKTA_BASE_PATH = "https://dev-2337597.okta.com"
-USER_INFO_ENDPOINT = "/oauth2/v1/userinfo"
+USER_INFO_ENDPOINT = "/oauth2/default/v1/userinfo"
 
 
 def get_user_profile(auth_header):
@@ -15,4 +15,5 @@ def get_user_profile(auth_header):
     if response.status_code == 200:
         return response.json()
     else:
+        print(response.status_code)
         return None
--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sam build --use-container
 sam local start-api --docker-network local-dynamodb \
 --skip-pull-image --profile default \
 --parameter-overrides 'ParameterKey=StageName,ParameterValue=local' \
