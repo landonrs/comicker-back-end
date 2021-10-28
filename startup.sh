6a72705ed2bebf5b4b65406a3e035e0dc4fff0aa
landon-shumway <landon.shumway@familysearch.org>
2021-10-28 15:29:40 -0500

    Adding role/creds for lambdas
    This role should be used when lambdas are deployed.
    
    When running locally, there should be a comicker-local profile
    set up in the creds file this will give us the needed s3 permissions

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sam build --use-container
 sam local start-api --docker-network local-dynamodb \
---skip-pull-image --profile default \
+--skip-pull-image --profile comicker-local \
 --parameter-overrides 'ParameterKey=StageName,ParameterValue=local' \
 -p 8080
