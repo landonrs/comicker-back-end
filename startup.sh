0627ebf77c4516b842b6f60490b2ca4b486ec55f
landon-shumway <landon.r.shumway@gmail.com>
2021-11-18 00:21:08 -0600

    Fix startup scripts
    we are no longer running on dynamo-db network

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export AWS_SDK_LOAD_CONFIG=true
 sam build --use-container
-sam local start-api --docker-network local-dynamodb \
+sam local start-api \
 --skip-pull-image --profile comicker-lambda-role \
 --parameter-overrides 'ParameterKey=System,ParameterValue=local' \
 -p 8080
